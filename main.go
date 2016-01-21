204a4794523e7a43ccfbdadde8292589f12b86c9
Stan Dyck <stan.dyck@gmail.com>
2016-01-20 20:26:02 -0800

    Adding notes.

--- main.go
+++ main.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	"gopl.io/ch2/popcount"
 	"gopl.io/ch2/tempconv"
 )
 
-func main() {
-	for _, arg := range os.Args[1:] {
+var HTTPClient *http.Client
 
+func arrays() {
+	/*	ab := [...]int{1, 2, 3} */
+	a := [3]int{1, 2}
+	fmt.Println(a[0])
+
+	for i, v := range a {
+		fmt.Printf("%d %d\n", i, v)
 	}
+}
+
+func othercrap() {
 	f := tempconv.Fahrenheit(23)
 	fmt.Fprintf(os.Stderr, "cf: %v\n", f)
 	fmt.Fprintf(os.Stderr, "Freezing temp is %v\n", tempconv.FreezingC)
 	fmt.Fprintf(os.Stdout, "celsius: %v\n", tempconv.FToC(f))
+	HTTPClient = http.DefaultClient
+	req, err := http.NewRequest("GET", "http://www.standyck.com", nil)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "err: %v\n", err)
+	} else {
+		r, _ := HTTPClient.Do(req)
+		fmt.Fprintf(os.Stdout, "req: %v\n", req)
+		fmt.Fprintf(os.Stdout, "response: %v\n", r)
+	}
 
+	z := popcount.PopCount(19235)
+	fmt.Printf("popcount: %v\n", z)
+}
+
+func main() {
+	arrays()
 }
